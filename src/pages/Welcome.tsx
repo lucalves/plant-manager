f1b91082f565c3fc55599d0a65206385a48663c3
Lucas Alves <lcsjalves@gmail.com>
2021-04-22 22:20:08 -0300

    Initial commit

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { 
+  SafeAreaView, 
+  View,
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet, 
+  Dimensions 
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+
+export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate('UserIdentification');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          de forma fácil
+        </Text>
+
+        <Image 
+          source={wateringImg} 
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather 
+            name="chevron-right" 
+            style={styles.buttonIcon} 
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 38,
+    lineHeight: 34
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
+  }
+});
